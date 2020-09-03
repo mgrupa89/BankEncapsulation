29137a4fc383311fb73f73f0e1b584175c1a01d2
mgrupa89 <69479947+mgrupa89@users.noreply.github.com>
2020-09-03 11:52:20 -0500

    Finished Bank Encapsulation Exercise. have a functioning ATM

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    class BankAccount
+    {
+        private double balance;
+
+        public BankAccount(double currentBalance = 0)
+        {
+            balance = currentBalance;
+
+        }
+        public void Deposit(double currentBalance)
+        {
+ 
+            balance += currentBalance;
+        }
+
+        public void Withdraw(double currentBalance)
+        {
+            balance -= currentBalance;
+           
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace BankEncapsulation
 {
@@ -6,7 +7,47 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            bool quit = false;
+            Console.WriteLine("Welcome to our bank!");
+
+            BankAccount bankAccount = new BankAccount();
+           
+            do
+            {
+                Console.WriteLine($"Please enter one of the follow choices. \n" + "Balance \n" + "Deposit \n" + "Withdraw \n" + "Quit");
+
+                string option = (Console.ReadLine().ToLower().Trim());
+
+                if (option == "balance")
+                {
+                    Console.WriteLine("Your current balance:");
+                    bankAccount.GetBalance();
+                    Console.WriteLine($"${bankAccount.GetBalance()}");
+                }
+                else if (option == "deposit")
+                {
+                    Console.WriteLine("How much would you like to deposit?");
+                    var amount = Convert.ToDouble(Console.ReadLine());
+
+                    bankAccount.Deposit(amount);
+                    Console.WriteLine($"Your new balance is ${bankAccount.GetBalance()}");
+                }
+                else if (option == "withdraw")
+                {
+
+                    Console.WriteLine("How much would you like to withdraw?");
+                    var amount = Convert.ToDouble(Console.ReadLine());
+                    bankAccount.Withdraw(amount);
+                    Console.WriteLine($"Your new balance is ${bankAccount.GetBalance()}");
+                }
+                else  if (option == "quit")
+                {
+                    quit = true;
+                    Console.WriteLine("Thank you for your patronage. \n" + "Goodbye");
+                }
+            } while (quit == false);
         }
-    }
+    }  
+
 }
